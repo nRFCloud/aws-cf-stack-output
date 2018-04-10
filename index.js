91bc2a8cc3d112adead0b2795550cac595b94f18
Markus Tacker <Markus.Tacker@NordicSemi.no>
2018-04-10 15:34:30 +0200

    refactor(node): make work with node 6

--- index.js
+++ index.js
@@ -2,19 +2,17 @@
 
 const { CloudFormation } = require('aws-sdk');
 
-const describeStack = async (cf, StackName) => {
-  const { Stacks } = await cf.describeStacks({ StackName }).promise();
-  const { Outputs } = Stacks.pop();
-  return Outputs.reduce((outputs, { OutputKey, OutputValue }) => {
+const describeStack = (cf, StackName) => cf.describeStacks({ StackName }).promise()
+  .then(({ Stacks }) => Stacks.pop())
+  .then(({ Outputs }) => Outputs.reduce((outputs, { OutputKey, OutputValue }) => {
     outputs[OutputKey] = OutputValue;
     return outputs;
-  }, {});
-};
+  }, {}));
 
-(async () => {
-  const stackName = process.argv[process.argv.length - 2];
-  const output = process.argv[process.argv.length - 1];
-  const cf = new CloudFormation();
-  const Outputs = await describeStack(cf, stackName);
-  console.log(Outputs[output]);
-})();
+const stackName = process.argv[process.argv.length - 2];
+const output = process.argv[process.argv.length - 1];
+const cf = new CloudFormation();
+describeStack(cf, stackName)
+  .then(Outputs => {
+    console.log(Outputs[output]);
+  });
