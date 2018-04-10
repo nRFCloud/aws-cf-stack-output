37937a7ba7fb241afb10f77e6a3b5dc123ab2e24
Markus Tacker <Markus.Tacker@NordicSemi.no>
2018-04-10 12:34:52 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,18 @@
+const { CloudFormation } = require('aws-sdk');
+
+const describeStack = async (cf, StackName) => {
+  const { Stacks } = await cf.describeStacks({ StackName }).promise();
+  const { Outputs } = Stacks.pop();
+  return Outputs.reduce((outputs, { OutputKey, OutputValue }) => {
+    outputs[OutputKey] = OutputValue;
+    return outputs;
+  }, {});
+};
+
+(async () => {
+  const stackName = process.argv[process.argv.length - 2];
+  const output = process.argv[process.argv.length - 1];
+  const cf = new CloudFormation();
+  const Outputs = await describeStack(cf, stackName);
+  console.log(Outputs[output]);
+})();
